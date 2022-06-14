715bce3deaf44480ef6bf6b93849c718e6c4b90a
Mohamed Ragab <mohamed.tawfieq@asgatech.com>
2022-06-14 12:37:56 +0200

    filters

--- src/Content/Content.tsx
+++ src/Content/Content.tsx
@@ -1,37 +1,10 @@
 import React from 'react'
+import Filters from '../Filters/Filters.tsx'
 
 function Content() {
     return (
-        <div className="px-5">
-            <table className="table">
-                <thead>
-                    <tr>
-                        <th scope="col">#</th>
-                        <th scope="col">First</th>
-                        <th scope="col">Last</th>
-                        <th scope="col">Handle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <th scope="row">1</th>
-                        <td>Mark</td>
-                        <td>Otto</td>
-                        <td>@mdo</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">2</th>
-                        <td>Jacob</td>
-                        <td>Thornton</td>
-                        <td>@fat</td>
-                    </tr>
-                    <tr>
-                        <th scope="row">3</th>
-                        <td colSpan={2}>Larry the Bird</td>
-                        <td>@twitter</td>
-                    </tr>
-                </tbody>
-            </table>
+        <div className="px-2 px-lg-5">
+            <Filters />
         </div>
     )
 }
---
+++ src/Filters/Filters.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import searchIcon from '../images/icons/Search.svg'
+
+function Filters() {
+    return (
+        <div className="filters">
+            <div className="filter search">
+                <label>ابحث عن كود الموعد, اسم الخدمة أو اسم الفرع</label>
+                <div className="input-group">
+                    <input type="text" className="form-control" placeholder="ابحث" />
+                    <span className="input-group-text"><img src={searchIcon} alt="" /></span>
+                </div>
+            </div>
+            <div className="filter">
+                <label>الخدمة</label>
+                <select className="form-select" aria-label="select">
+                    <option selected>الكل</option>
+                    <option value="1">فك رهن</option>
+                    <option value="2">طلب تمويل</option>
+                </select>
+            </div>
+            <div className="filter">
+                <label>الفرع</label>
+                <select className="form-select" aria-label="select">
+                    <option selected>الكل</option>
+                    <option value="1">جدة</option>
+                    <option value="2">الرياض</option>
+                </select>
+            </div>
+            <div className="filter">
+                <label>التاريخ من/إلي</label>
+                <input type="text" className='form-control' />
+            </div>
+        </div>
+    )
+}
+
+export default Filters
