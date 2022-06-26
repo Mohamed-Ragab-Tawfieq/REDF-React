dcf9c895930502f5e13335f836d3ceb6b34942f0
Mohamed Ragab <mohamed.tawfieq@asgatech.com>
2022-06-26 18:30:28 +0200

    header new buttons

--- src/theme/Components/Header/Header.tsx
+++ src/theme/Components/Header/Header.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { NavLink } from 'react-router-dom';
 
 import printIcon from '../../../assets/images/icons/print.svg';
+import earthIcon from '../../../assets/images/icons/earth.svg'
+import notificationIcon from '../../../assets/images/icons/notification.svg'
+import logoutIcon from '../../../assets/images/icons/logout.svg'
+import userIcon from '../../../assets/images/profile.png'
 
 function Header() {
     return (
@@ -18,6 +22,70 @@ function Header() {
             <div className="actions">
                 <button className='btn excel-btn'>تصدير كملف Excel</button>
                 <button className='btn print-btn'>طباعة <img src={printIcon} alt="" /></button>
+            </div>
+
+            <div className="notifications">
+                <div className="btns-wrap btn-group">
+                    <div className="btn-group">
+                        <button type="button" className="btn lang-btn" id="langDrodownBtn" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src={earthIcon} alt="" />
+                        </button>
+
+                        <ul className="dropdown-menu lang" aria-labelledby="langDrodownBtn">
+                            <li className="dropdown-item form-check">
+                                <input className="form-check-input" type="radio" name="langCheck" id="arLang" />
+                                <label className="form-check-label" htmlFor="arLang">
+                                    اللغة العربية
+                                </label>
+                            </li>
+                            <li className="dropdown-item form-check">
+                                <input className="form-check-input" type="radio" name="langCheck" id="enLang" />
+                                <label className="form-check-label" htmlFor="enLang">
+                                    اللغة الإنجليزية
+                                </label>
+                            </li>
+                        </ul>
+                    </div>
+                    <div className="btn-group">
+                        <button type="button" className="btn" id="notificationsDrodownBtn" data-bs-toggle="dropdown" aria-expanded="false">
+                            <img src={notificationIcon} alt="" />
+                        </button>
+
+                        <ul className="dropdown-menu" aria-labelledby="notificationsDrodownBtn">
+                            <li><a className="dropdown-item" href="#">Action</a></li>
+                            <li><a className="dropdown-item" href="#">Another action</a></li>
+                            <li><a className="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+
+            <div className="profile dropdown">
+                <button className="btn dropdown-toggle" type="button" id="userMenuBtn" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src={userIcon} alt="" />
+                    <div className="d-flex flex-column mx-3">
+                        <span>أحمد سمير</span>
+                        <small>مدير خدمة حجز موعد</small>
+                    </div>
+                </button>
+
+                <ul className="dropdown-menu profile" aria-labelledby="userMenuBtn">
+                    <li className='dropdown-item'>
+                        <NavLink to="user">
+                            <strong>أحمد سمير محمود علي</strong>
+                            <small>مدير خدمة حجز موعد</small>
+                        </NavLink>
+                    </li>
+                    <li className='dropdown-item email'>
+                        ServiceManager@REDF.com
+                    </li>
+                    <li className='dropdown-item'>
+                        <NavLink to="logout">
+                            <img src={logoutIcon} className="logout-icon" alt="" />
+                            تسجيل الخروج
+                        </NavLink>
+                    </li>
+                </ul>
             </div>
         </header>
     )
